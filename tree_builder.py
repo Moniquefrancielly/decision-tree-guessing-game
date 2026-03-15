90d1ed5e0e6eb419d24a345af7233bba328fcc29
delaureanlaura <delaureanlaura@hotmail.com>
2026-03-15 17:21:27 -0300

    refactor: traduz atributos do tree_builder para inglês e garante compatibilidade

--- tree_builder.py
+++ tree_builder.py
@@ -1,80 +1,80 @@
 from node import Node
 
 def build_tree():
-    root = Node(pergunta="É doce?") # a raiz do sistema
+    root = Node(question="É doce?") # a raiz do sistema
 
-    # Parte DOCE do (SIM)
-    doce_gelado = Node(pergunta="É gelado?")
-    root.sim = doce_gelado
+    # Parte DOCE (yes)
+    doce_gelado = Node(question="É gelado?")
+    root.yes = doce_gelado
 
     # Sub-ramo Gelado
-    gelado_sorvete = Node(pergunta="É sorvete?")
-    doce_gelado.sim = gelado_sorvete
-    gelado_sorvete.sim = Node(resposta="Sorvete")
+    gelado_sorvete = Node(question="É sorvete?")
+    doce_gelado.yes = gelado_sorvete
+    gelado_sorvete.yes = Node(answer="Sorvete")
     
-    gelado_pave = Node(pergunta="É pavê?")
-    gelado_sorvete.nao = gelado_pave
-    gelado_pave.sim = Node(resposta="Pavê")
-    gelado_pave.nao = Node(resposta="Pudim")
+    gelado_pave = Node(question="É pavê?")
+    gelado_sorvete.no = gelado_pave
+    gelado_pave.yes = Node(answer="Pavê")
+    gelado_pave.no = Node(answer="Pudim")
 
     # Sub-ramo não gelado
-    tem_chocolate = Node(pergunta="Tem chocolate?")
-    doce_gelado.nao = tem_chocolate
+    tem_chocolate = Node(question="Tem chocolate?")
+    doce_gelado.no = tem_chocolate
     
-    choc_bolo = Node(pergunta="É bolo?")
-    tem_chocolate.sim = choc_bolo
-    choc_bolo.sim = Node(resposta="Bolo de chocolate")
+    choc_bolo = Node(question="É bolo?")
+    tem_chocolate.yes = choc_bolo
+    choc_bolo.yes = Node(answer="Bolo de chocolate")
     
-    choc_brigadeiro = Node(pergunta="É brigadeiro?")
-    choc_bolo.nao = choc_brigadeiro
-    choc_brigadeiro.sim = Node(resposta="Brigadeiro")
-    choc_brigadeiro.nao = Node(resposta="Sonho")
+    choc_brigadeiro = Node(question="É brigadeiro?")
+    choc_bolo.no = choc_brigadeiro
+    choc_brigadeiro.yes = Node(answer="Brigadeiro")
+    choc_brigadeiro.no = Node(answer="Sonho")
 
     # Sub-ramo sem chocolate
-    biscoito_doce = Node(pergunta="É biscoito?")
-    tem_chocolate.nao = biscoito_doce
-    biscoito_doce.sim = Node(resposta="Biscoito doce")
+    biscoito_doce = Node(question="É biscoito?")
+    tem_chocolate.no = biscoito_doce
+    biscoito_doce.yes = Node(answer="Biscoito doce")
     
-    vai_forno = Node(pergunta="Vai ao forno?")
-    biscoito_doce.nao = vai_forno
-    vai_forno.sim = Node(resposta="Tortelete")
+    vai_forno = Node(question="Vai ao forno?")
+    biscoito_doce.no = vai_forno
+    vai_forno.yes = Node(answer="Tortelete")
     
-    festinhas = Node(pergunta="Tem em festinhas?")
-    vai_forno.nao = festinhas
-    festinhas.sim = Node(resposta="Beijinho")
-    festinhas.nao = Node(resposta="Doce desconhecido")
+    festinhas = Node(question="Tem em festinhas?")
+    vai_forno.no = festinhas
+    festinhas.yes = Node(answer="Beijinho")
+    festinhas.no = Node(answer="Doce desconhecido")
 
-    # Parte SALGADO do (NÃO)
-    frito = Node(pergunta="É frito?")
-    root.nao = frito
+    # Parte SALGADO (no)
+    frito = Node(question="É frito?")
+    root.no = frito
 
     # Sub-ramo frito
-    tem_recheio = Node(pergunta="Tem recheio?")
-    frito.sim = tem_recheio
+    tem_recheio = Node(question="Tem recheio?")
+    frito.yes = tem_recheio
     
-    coxinha = Node(pergunta="É coxinha?")
-    tem_recheio.sim = coxinha
-    coxinha.sim = Node(resposta="Coxinha")
-    coxinha.nao = Node(resposta="Pastel")
-    tem_recheio.nao = Node(resposta="Batata frita")
+    coxinha = Node(question="É coxinha?")
+    tem_recheio.yes = coxinha
+    coxinha.yes = Node(answer="Coxinha")
+    coxinha.no = Node(answer="Pastel")
+    tem_recheio.no = Node(answer="Batata frita")
 
     # Sub-ramo não frito
-    tem_pao = Node(pergunta="Tem pão?")
-    frito.nao = tem_pao
+    tem_pao = Node(question="Tem pão?")
+    frito.no = tem_pao
     
-    tem_hamburguer = Node(pergunta="Tem hambúrguer?")
-    tem_pao.sim = tem_hamburguer
-    tem_hamburguer.sim = Node(resposta="Hambúrguer")
-    tem_hamburguer.nao = Node(resposta="Passaporte") 
+    tem_hamburguer = Node(question="Tem hambúrguer?")
+    tem_pao.yes = tem_hamburguer
+    tem_hamburguer.yes = Node(answer="Hambúrguer")
+    tem_hamburguer.no = Node(answer="Passaporte")
 
     # Sub-ramo sem pão
-    polvilho = Node(pergunta="É feito com polvilho?")
-    tem_pao.nao = polvilho
-    polvilho.sim = Node(resposta="Pão de queijo")
+    polvilho = Node(question="É feito com polvilho?")
+    tem_pao.no = polvilho
+    polvilho.yes = Node(answer="Pão de queijo")
     
-    frigideira = Node(pergunta="É feito na frigideira?")
-    polvilho.nao = frigideira
-    frigideira.sim = Node(resposta="Tapioca")
-    frigideira.nao = Node(resposta="Torta")
+    frigideira = Node(question="É feito na frigideira?")
+    polvilho.no = frigideira
+    frigideira.yes = Node(answer="Tapioca")
+    frigideira.no = Node(answer="Torta")
 
     return root
